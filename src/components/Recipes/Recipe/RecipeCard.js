49e377929f54a624758597271c86cef233466a76
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-16 13:54:46 -0500

    recipe card list and recipe card accessing context

--- src/components/Recipes/Recipe/RecipeCard.js
+++ src/components/Recipes/Recipe/RecipeCard.js
@@ -1,13 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './RecipeCard.css';
 
 export default class RecipeCard extends Component {
   render() {
+    const { recipe } = this.props
     return (
-      <section class="recipe-card">
-        <h2>Recipe Title</h2>
-        <p>Category: Recipe Category</p>
-        <p>Rating: Recipe Rating</p>
+      <section className='RecipeCard'>
+          <header className='RecipeCard__header'>
+          <Link to={`/recipe/${recipe.id}`} className='RecipeCard__recipelink'>
+          <h3 className='RecipeCard__heading'>{recipe.name}</h3>
+          </Link >
+          <p>Category: Recipe Category</p>
+          <p>Rating: Recipe Rating</p>
+        </header>
       </section>
+      
     )
   };
 };
--- src/components/Recipes/RecipeCardList.js
+++ src/components/Recipes/RecipeCardList.js
@@ -3,6 +3,7 @@ import RecipeListContext from '../../contexts/RecipeCardListContext';
 // import RecipeCard from './Recipe/RecipeCard';
 import RecipeApiService from '../../services/recipes-api-service';
 import { Section } from '../../components/Utils/Utils';
+import RecipeCard from './Recipe/RecipeCard';
 
 export default class RecipeCardList extends Component {
 
@@ -18,10 +19,10 @@ export default class RecipeCardList extends Component {
   renderRecipes() {
     const { recipeList = [] } = this.context;
     return recipeList.map(recipe => 
-      <div>
-        <h2>I'm a recipe card for {recipe.name}!</h2>
-        <p></p>
-      </div>
+      <RecipeCard
+        key={recipe.id}
+        recipe={recipe}
+        />
       )
   }
 
