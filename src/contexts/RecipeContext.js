5cc8eb10ca37d28568ff814c9b5572b6548de4c3
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-16 13:55:06 -0500

    initial commit for recipe context

---
+++ src/contexts/RecipeContext.js
@@ -0,0 +1,57 @@
+import React, { Components } from 'react'
+
+export const nullRecipe = {
+  
+}
+
+const RecipeContext = React.createContext({
+  recipe: nullRecipe,
+  error: null,
+  setError: () => {},
+  clearError: () => {},
+  setRecipe: () => {},
+  clearRecipe: () => {},
+})
+
+export default RecipeContext;
+
+export class RecipeProvider extends Component {
+  state={
+    recipe: nullRecipe,
+    error: null,
+  }
+};
+
+setError = error => {
+  console.error(error)
+  this.setState({error})
+}
+
+clearError = () => {
+  this.setState({error:null})
+}
+
+setRecipe = recipe => {
+  this.setState({recipe})
+}
+
+clearRecipe = () => {
+  this.setRecipe(nullRecipe)
+}
+
+render() {
+  const value = {
+    recipe: this.state.recipe,
+    error: this.state.error,
+    //TODO Ingredients here
+    setError: this.setError,
+    clearError: this.clearError,
+    setRecipe: this.setRecipe,
+    clearRecipe: this.clearRecipe
+  }
+  return (
+    <RecipeContext.Provider value={value}>
+      {this.props.children}
+    </RecipeContext.Provider>
+  )
+}
