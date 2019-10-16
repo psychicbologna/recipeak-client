357d1c55097560cc4b38580703c7476c2faf520e
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-16 13:55:22 -0500

    dummy data ingredient

--- src/components/Recipes/Recipe/IngredientsList.js
+++ src/components/Recipes/Recipe/IngredientsList.js
@@ -4,7 +4,9 @@ export default class PrimaryNav extends Component {
   render() {
     return (
       <ul className='primarynav'>
-        <li>Map function for links goes here.</li>
+        <li>I am a fake ingredient item!</li>
+        <li>We aren't implemented yet!</li>
+        <li>Why are you dawdling?</li>
       </ul>
     )
   }
