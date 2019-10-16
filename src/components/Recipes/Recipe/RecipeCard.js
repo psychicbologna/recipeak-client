46cc88283c4a633a3a2f73375a0497979fd0c5b6
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-16 14:48:00 -0500

    recipe card cleanup, added dates

--- src/components/Recipes/Recipe/RecipeCard.js
+++ src/components/Recipes/Recipe/RecipeCard.js
@@ -3,19 +3,31 @@ import { Link } from 'react-router-dom';
 import './RecipeCard.css';
 
 export default class RecipeCard extends Component {
+
+  renderRecipeCardFooter() {
+    const { recipe } = this.props
+    return (
+      <footer className='RecipeCard__footer'>
+        {!recipe.date_updated ? 'Created: ' + recipe.date_created : 'Last Updated: ' + recipe.date_updated}
+      </footer>
+    )
+  }
+
   render() {
     const { recipe } = this.props
     return (
       <section className='RecipeCard'>
-          <header className='RecipeCard__header'>
+        <header className='RecipeCard__header'>
           <Link to={`/recipe/${recipe.id}`} className='RecipeCard__recipelink'>
-          <h3 className='RecipeCard__heading'>{recipe.name}</h3>
+            <h3 className='RecipeCard__heading'>{recipe.name}</h3>
           </Link >
-          <p>Category: Recipe Category</p>
-          <p>Rating: Recipe Rating</p>
+          <h4>by {recipe.author}</h4>
         </header>
+        <p>Category: Recipe Category</p>
+        <p>Rating: Recipe Rating</p>
+        {this.renderRecipeCardFooter()}
       </section>
-      
+
     )
   };
 };
