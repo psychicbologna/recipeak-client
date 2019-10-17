16a0725df639a1096813295ab76b490364672f44
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-17 13:29:20 -0500

    buggy with rendering but structured

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import LoginForm from '../../components/forms/LoginForm/LoginForm';
 
 export default class LoginPage extends Component {
+  static defaultProps = {
+    location: {},
+    history: {
+      push: () => {},
+    },
+  }
+
+  handleLoginSuccess = () => {
+    //Track history, move to '/recipes' on successful login
+    const { location, history } = this.props;
+    const destination = (location.state || {}).from || '/recipes';
+    history.push(destination);
+  }
+
   render() {
     return (
-      <div>
-        Whoop
-      </div>
+      <section className='LoginPage'>
+        <LoginForm
+          onLoginSuccess={this.handleLoginSuccess}
+        />
+      </section>
     )
   }
 }
