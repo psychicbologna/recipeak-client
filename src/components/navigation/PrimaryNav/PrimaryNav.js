4157c6209846a800ac89683bf1a67c819df799a2
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-17 13:30:46 -0500

    updated with links but bugs w/ logged in/out

--- src/components/navigation/PrimaryNav/PrimaryNav.js
+++ src/components/navigation/PrimaryNav/PrimaryNav.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import './PrimaryNav.css';
 import '../navigation.css';
-//TODO after TokenService, conditional rendering for logged in/not logged in
-//TODO <Links> with router-dom
+import TokenService from '../../../services/token-service';
+import { Link } from 'react-router-dom';
 
-//Stateful component
+//TODO the renders for login/logout link are not loading/working properly
 
 export default class PrimaryNav extends Component {
+  handleLogoutClick = () => {}
+
+  renderLoginLink() {
+    return (
+      <li>
+        <Link to='/signup'>Sign Up</Link>
+        <Link to='/login'>Log In</Link>
+      </li>
+    )
+  }
+  renderLogoutLink() {
+    return (
+      <li>
+        <Link onClick={this.handleLogoutClick} to='/' >
+          Log Out
+        </Link>
+      </li>
+    )
+  }
   render() {
     return (
       <ul className='primary-nav'>
-        <li>Sign Up</li>
         <li>Logo</li>
-        <li>Log In</li>
-        <li>About</li>
+        {
+          TokenService.hasAuthToken()
+            ? this.renderLogoutLink()
+            : this.renderLoginLink()
+        }
+        <li>
+          <Link to='/about'>
+            About
+          </Link>
+        </li>
       </ul>
     )
   }
