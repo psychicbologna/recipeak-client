5a4b872252f6cec5b812d1080d8c97bd0aeb2af8
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-17 13:28:53 -0500

    changed jsx format

--- src/routes/UserHome/UserHome.js
+++ src/routes/UserHome/UserHome.js
@@ -5,11 +5,11 @@ import RecipeCardList from '../../components/Recipes/RecipeCardList';
 export default class UserHome extends Component {
   render() {    
     return (
-      <main>
+      <section className='UserHome'>
         <h2>[Name]'s Recipes</h2>
         <UserHomeNav />
         <RecipeCardList />
-      </main>
+      </section>
     )
   }
 }
