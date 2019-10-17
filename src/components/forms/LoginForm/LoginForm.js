f7f07b827c5fcc105c9ca7cdaf7c7388ba1f8b9d
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-17 13:30:06 -0500

    successfully logs in

--- src/components/forms/LoginForm/LoginForm.js
+++ src/components/forms/LoginForm/LoginForm.js
@@ -1,17 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import TokenService from '../../../services/token-service';
+import { Button, Input } from '../../Utils/Utils';
 
 export default class LoginForm extends Component {
+  static defaultProps = {
+    onLoginSuccess: () => { }
+  };
+
+  state = { error: null };
+
+  handleSubmitBasicAuth = event => {
+    event.preventDefault();
+    const { user_name, password } = event.target;
+
+    TokenService.saveAuthToken(
+      TokenService.makeBasicAuthToken(user_name.value, password.value)
+    );
+
+    user_name.value = '';
+    password.value = '';
+    this.props.onLoginSuccess();
+  }
+
   render() {
+    const { error } = this.state;
     return (
-      <section className="signup-form">
-      <form id="signup">
-        <label for="username">Username</label>
-        <input type="text" name="username" />
-        <label for="password">Password</label>
-        <input type="text" name="password" />
-        <button type="submit">Submit</button>
+      <form className='LoginForm' onSubmit={this.handleSubmitBasicAuth}>
+        <div role='alert'>
+          {error && <p className='error__p'>{error}</p>}
+        </div>
+        <label htmlFor='LoginForm__username'>
+          Username
+            </label>
+        <Input
+          name='user_name'
+          id='LoginForm__user_name'>
+        </Input>
+        <label htmlFor='LoginForm__password'>Password</label>
+        <Input
+          name='password'
+          id='LoginForm__password' />
+        <Button type='submit'>
+          Login
+        </Button>
       </form>
-    </section>
     )
   }
 }
