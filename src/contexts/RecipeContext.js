8688ad328710be669c33c8bc632e8fe3c0396eda
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-18 04:47:13 -0500

    beginning to refactor for ingredients

--- src/contexts/RecipeContext.js
+++ src/contexts/RecipeContext.js
@@ -1,57 +1,74 @@
-import React, { Components } from 'react'
+import React, { Component } from 'react'
 
 export const nullRecipe = {
-  
+//TODO what will go here? User? categories?
 }
 
 const RecipeContext = React.createContext({
   recipe: nullRecipe,
+  ingredients: [],
   error: null,
-  setError: () => {},
-  clearError: () => {},
-  setRecipe: () => {},
-  clearRecipe: () => {},
+  setError: () => { },
+  clearError: () => { },
+  setRecipe: () => { },
+  clearRecipe: () => { },
+  setIngredients: () => { },
+  addIngredient: () => { }
+  //TODO change/delete ingredient
 })
 
 export default RecipeContext;
 
 export class RecipeProvider extends Component {
-  state={
+  state = {
     recipe: nullRecipe,
     error: null,
   }
-};
 
-setError = error => {
-  console.error(error)
-  this.setState({error})
-}
 
-clearError = () => {
-  this.setState({error:null})
-}
-
-setRecipe = recipe => {
-  this.setState({recipe})
-}
-
-clearRecipe = () => {
-  this.setRecipe(nullRecipe)
-}
-
-render() {
-  const value = {
-    recipe: this.state.recipe,
-    error: this.state.error,
-    //TODO Ingredients here
-    setError: this.setError,
-    clearError: this.clearError,
-    setRecipe: this.setRecipe,
-    clearRecipe: this.clearRecipe
+  setError = error => {
+    console.error(error)
+    this.setState({ error })
   }
-  return (
-    <RecipeContext.Provider value={value}>
-      {this.props.children}
-    </RecipeContext.Provider>
-  )
-}
+
+  clearError = () => {
+    this.setState({ error: null })
+  }
+
+  setRecipe = recipe => {
+    this.setState({ recipe })
+  }
+
+  clearRecipe = () => {
+    this.setRecipe(nullRecipe)
+  }
+
+  setIngredients = ingredients => {
+    this.setState({ ingredients })
+  }
+
+  addIngredient = ingredient => {
+    this.setIngredients([
+      ...this.state.recipe.ingredients,
+      ingredient
+    ])
+  }
+
+  render() {
+    const value = {
+      recipe: this.state.recipe,
+      // ingredients: this.recipe.ingredients,
+      error: this.state.error,
+      setError: this.setError,
+      clearError: this.clearError,
+      setRecipe: this.setRecipe,
+      clearRecipe: this.clearRecipe
+    };
+
+    return (
+      <RecipeContext.Provider value={value}>
+        {this.props.children}
+      </RecipeContext.Provider>
+    )
+  }
+};
