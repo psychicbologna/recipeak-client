1702323a587ce86cb8a6e6df3e87279983173a76
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-20 17:46:57 -0500

    form structure added

--- src/components/forms/SignupForm/SignupForm.js
+++ src/components/forms/SignupForm/SignupForm.js
@@ -1,23 +1,66 @@
 import React, { Component } from 'react';
+import { Button, Input } from '../../Utils/Utils';
 
-export default class RegistrationForm extends Component {
+export default class SignupForm extends Component {
+
+  static defaultProps = {
+    onSignupSuccess: () => { }
+  }
+
+  state = { error: null }
+
+  handleSubmit = ev => {
+    ev.preventDefault()
+    const { first_name, last_name, username, password } = ev.target;
+
+    console.log('Signup Form submitted...')
+    console.log({ first_name, last_name, username, password })
+
+    first_name.value = '';
+    last_name.value = '';
+    username.value = '';
+    password.value = '';
+    this.props.onSignupSuccess();
+  }
+
   render() {
+    const { error } = this.state;
+
     return (
-      <section className="signup-form">
-        <form id="signup">
-          <label for="name">Name</label>
-          <input type="text" name="name" />
-          <label for="username">Username</label>
-          <input type="text" name="username" />
-          <label for="email">Email</label>
-          <input type="text" name="name" />
-          <label for="password">Password</label>
-          <input type="text" name="password" />
-          <label for="password">Reenter Password</label>
-          <input type="text" name="password" />
-          <button type="submit">Submit</button>
-        </form>
-      </section >
+      <form className='SignupForm' onSubmit={this.handleSubmit}>
+        <div role='alert'>
+          {error && <p className='error__p'>{error}</p>}
+        </div>
+        <label htmlFor='SignUpForm__first_name'>First Name</label>
+        <Input
+          type='text'
+          name='first_name'
+          id='SignUpForm__first_name'>
+        </Input>
+        <label htmlFor='SignUpForm__last_name'>Last Name</label>
+        <Input
+          type='text'
+          name='last_name'
+          id='SignUpForm__last_name'>
+        </Input>
+        <label htmlFor='SignUpForm__username'>Username</label>
+        <Input
+          type='text'
+          name='username'
+          id='SignUpForm__username'>
+        </Input>
+        <label htmlFor='password'>Password</label>
+        <Input
+          type='text'
+          name='password'
+          id='SignUpForm__password'>
+        </Input>
+        {/*TODO Reenter password input */}
+
+        <Button type='submit'>
+          Sign Up!
+        </Button>
+      </form>
     )
   };
 };
