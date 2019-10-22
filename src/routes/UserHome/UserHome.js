9e671853adfcdb4a2acc36d9778274a745840b4c
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-22 02:07:54 -0500

    header fix

--- src/routes/UserHome/UserHome.js
+++ src/routes/UserHome/UserHome.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import UserHomeNav from '../../components/navigation/UserHomeNav/UserHomeNav'
 import RecipeCardList from '../../components/Recipes/RecipeCardList';
+import TokenService from '../../services/token-service';
 
+//TODO 'Your' to ':Name'
 export default class UserHome extends Component {
-  render() {    
+  render() {
     return (
       <section className='UserHome'>
-        <h2>[Name]'s Recipes</h2>
+        <h2>Your Recipes</h2>
         <UserHomeNav />
         <RecipeCardList />
       </section>
