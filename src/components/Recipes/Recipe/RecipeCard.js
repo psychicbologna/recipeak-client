ae3ab1cb8b9e3f92c2a2d52af69a5410a7138f1b
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-23 00:40:22 -0500

    date format fixed

--- src/components/Recipes/Recipe/RecipeCard.js
+++ src/components/Recipes/Recipe/RecipeCard.js
@@ -4,19 +4,25 @@ import { Link } from 'react-router-dom';
 import './RecipeCard.css';
 
 export default class RecipeCard extends Component {
-
+  
   RecipeDate(prepend, date) {
-    return (
-      <p className='RecipeCard__date'>{prepend}: {NiceDate(JSON.stringify(date))}</p>
+    const dateObject = new Date(date);
+    const dateContent = `${prepend}: ${dateObject.toLocaleDateString()} at ${dateObject.toLocaleTimeString()}`;
+
+    if (date) {
+        return (
+      <p className='RecipeCard__date'>{dateContent}</p>
     )
-  } 
+    }
+  }
 
   renderRecipeCardFooter() {
     const { recipe } = this.props
+
     return (
       <footer className='RecipeCard__footer'>
-        { this.RecipeDate('Created', recipe.date_created) }
-        { this.RecipeDate('Last Updated', recipe.date_updated) }
+        {this.RecipeDate('Created', recipe.date_created)}
+        {this.RecipeDate('Last Updated', recipe.date_updated)}
       </footer>
     )
   }
@@ -34,7 +40,7 @@ export default class RecipeCard extends Component {
         <p>Prep Time: {recipe.prep_time}</p>
         {/* <p>Category: Recipe Category</p>
         <p>Rating: Recipe Rating</p> */}
-        {/* {this.renderRecipeCardFooter()} */}
+        {this.renderRecipeCardFooter()}
       </section>
 
     )
