b41e64091177ec572a6601790287f7cf8c791f46
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-23 13:36:24 -0500

    sketched routes for api services edit/add/delete

--- src/services/recipes-api-service.js
+++ src/services/recipes-api-service.js
@@ -44,6 +44,40 @@ const RecipeApiService = {
       )
   },
 
+  deleteRecipe(recipeId) {
+    return fetch(`${config.API_ENDPOINT}/recipes/${recipeId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${TokenService.getAuthToken()}`,
+      }
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+
+  updateRecipe(recipeId, newRecipeFields) {
+    return fetch(`${config.API_ENDPOINT}/recipes/ingredients/${ingredientId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${TokenService.getAuthToken()}`,
+      body: JSON.stringify({
+        recipeId,
+        newRecipeFields
+      })
+      }
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+
   getRecipeIngredients(recipeId) {
     return fetch(`${config.API_ENDPOINT}/recipes/${recipeId}/ingredients/`, {
       headers: {
@@ -60,9 +94,29 @@ const RecipeApiService = {
 
   deleteRecipeIngredients(ingredientId) {
     return fetch(`${config.API_ENDPOINT}/recipes/ingredients/${ingredientId}`, {
+      method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${TokenService.getAuthToken()}`,
+      }
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+
+  updateRecipeIngredients(ingredientId, recipe_id, newIngredientFields) {
+    return fetch(`${config.API_ENDPOINT}/recipes/ingredients/${ingredientId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${TokenService.getAuthToken()}`,
+      body: JSON.stringify({
+        recipe_id,
+        newIngredientFields
+      })
       }
     })
       .then(res =>
