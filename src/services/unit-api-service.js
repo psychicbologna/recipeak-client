a0e925b6e66456fff4f0eafe71be784ba233aeef
Alexander Fukui <alexander.fukui@gmail.com>
2019-10-23 13:07:43 -0500

    finished promise

---
+++ src/services/unit-api-service.js
@@ -0,0 +1,20 @@
+//TODO finish api call to retrieve units
+
+import config from '../config';
+
+const UnitService = {
+  getUnits() {
+    return fetch(`${config.API_ENDPOINT}/units`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      );
+  }
+};
+
+export default UnitService;
