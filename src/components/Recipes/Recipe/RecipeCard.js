283e0c6f9ef66b4998ec4c0505499d90c809db74
Alexander Fukui <alexander.fukui@gmail.com>
2019-11-03 02:23:58 -0600

    updated component functions to prep for delete

--- src/components/Recipes/Recipe/RecipeCard.js
+++ src/components/Recipes/Recipe/RecipeCard.js
@@ -1,31 +1,8 @@
 import React, { Component } from 'react';
-import { NiceDate } from '../../Utils/Utils';
 import { Link } from 'react-router-dom';
 import './RecipeCard.css';
 
 export default class RecipeCard extends Component {
-  
-  RecipeDate(prepend, date) {
-    const dateObject = new Date(date);
-    const dateContent = `${prepend}: ${dateObject.toLocaleDateString()} at ${dateObject.toLocaleTimeString()}`;
-
-    if (date) {
-        return (
-      <p className='RecipeCard__date'>{dateContent}</p>
-    )
-    }
-  }
-
-  renderRecipeCardFooter() {
-    const { recipe } = this.props
-
-    return (
-      <footer className='RecipeCard__footer'>
-        {this.RecipeDate('Created', recipe.date_created)}
-        {this.RecipeDate('Last Updated', recipe.date_updated)}
-      </footer>
-    )
-  }
 
   render() {
     const { recipe } = this.props
@@ -40,9 +17,36 @@ export default class RecipeCard extends Component {
         <p>Prep Time: {recipe.prep_time}</p>
         {/* <p>Category: Recipe Category</p>
         <p>Rating: Recipe Rating</p> */}
-        {this.renderRecipeCardFooter()}
+        <RecipeCardFooter recipe={recipe} />
       </section>
 
     )
   };
 };
+
+function RecipeCardFooter(props) {
+  const { recipe } = props
+  const { date_created, date_updated } = recipe;
+
+  return (
+    <footer className='RecipeCard__footer'>
+      <RecipeDate prepend='Created' date={date_created}/>
+      <RecipeDate prepend='Last Updated' date={date_updated}/>
+    </footer>
+  )
+}
+
+function RecipeDate(props) {
+  const {prepend, date} = props
+
+  const dateObject = new Date(date);
+  const dateContent = `${prepend}: ${dateObject.toLocaleDateString()} at ${dateObject.toLocaleTimeString()}`;
+
+  if (date) {
+      return (
+    <p className='RecipeCard__date'>{dateContent}</p>
+  )
+  } else {
+    return null;
+  }
+}
