cf4f85fab1f168da58386280958539abfa23faf8
Alexander Fukui <alexander.fukui@gmail.com>
2019-11-11 04:21:14 -0600

    removed units prop

--- src/routes/RecipeAddPage/RecipeAddPage.js
+++ src/routes/RecipeAddPage/RecipeAddPage.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import RecipesAddForm from '../../components/forms/RecipesForm/RecipesAddForm';
 
 export default class RecipeAddPage extends Component {
+
   render() {
     return (
       <section className='RecipeAdd'>
         <h3>Add Recipe</h3>
-        <RecipesAddForm units={this.props.units} />
+        <RecipesAddForm />
       </section>
     )
   }
