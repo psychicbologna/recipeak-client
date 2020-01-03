b0774926fa9937fbd5856952bf49ae06f2e8613c
Alexander Fukui <alexander.fukui@gmail.com>
2020-01-03 12:17:56 -0600

    deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://arcane-garden-07719.herokuapp.com/api',
   TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'cc70a9f0-016c-4a7b-8260-f9800dcc89a7',
 }
 
